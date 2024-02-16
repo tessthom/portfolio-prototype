7f7ef9f1db401dbee5645f056c10dbfb7a41ffc9
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2024-02-16 11:08:34 -0500

    Update Vite config to generate separate bundles for multiple pages

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        portfolio: 'pages/portfolio.html',
+        contact: 'pages/contact.html',
+        resume: 'pages/resume.html'
+      }
+    }
+  }
+}
