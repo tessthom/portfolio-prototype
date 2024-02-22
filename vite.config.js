a10adb967fd48301cb9b557b972573ce8a9b324a
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2024-02-22 13:11:39 -0500

    Add CSS resets and config environment-based pathing for dev v WTCC server

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 export default {
+  base: process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/',
   build: {
     rollupOptions: {
       input: {
@@ -10,3 +11,18 @@ export default {
     }
   }
 }
+
+// THIS PATHING ONLY WORKS FOR WTCC SERVER:
+// export default {
+//   base: '/abthomas3/web287/Projects/Portfolio/',
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: 'index.html',
+//         portfolio: 'pages/portfolio.html',
+//         contact: 'pages/contact.html',
+//         resume: 'pages/resume.html'
+//       }
+//     }
+//   }
+// }
