2c100b548dd90fe54788ebc66fcee54a451841f1
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2024-02-28 18:24:47 -0500

    Rename contact.html to about.html

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,26 @@ export default {
         contact: 'pages/contact.html',
         resume: 'pages/resume.html'
       }
+    },
+    define: {
+      '%BASE_URL%': process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/'
     }
   }
 }
+
+// export default {
+//   base: process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/',
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: 'index.html',
+//         portfolio: 'pages/portfolio.html',
+//         contact: 'pages/contact.html',
+//         resume: 'pages/resume.html'
+//       }
+//     }
+//   }
+// }
 
 // THIS PATHING ONLY WORKS FOR WTCC SERVER:
 // export default {
