17e40ff5f2ab6bfa6d215d6d2ae282f8418b19f7
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2024-04-02 14:07:53 -0400

    Remove WTCC server base URL config to prep for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 // This conditional logic and placeholder account for the variation in root path between local dev server and the root when the project is uploaded to the school server.
+// export default {
+//   base: process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/',
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: 'index.html',
+//         portfolio: 'pages/portfolio.html',
+//         contact: 'pages/about.html',
+//         resume: 'pages/resume.html'
+//       }
+//     },
+//     define: {
+//       '%BASE_URL%': process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/'
+//     }
+//   }
+// }
+
 export default {
-  base: process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
@@ -11,25 +28,55 @@ export default {
       }
     },
     define: {
-      '%BASE_URL%': process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/'
+      '%BASE_URL%': '/'
     }
   }
 }
 
+/*
+  * The above defines environment variables to account for differences
+  * ...in root path between local dev server and WTCC server.
+  * The `proecess.env.NODE_ENV` variable determines if project is running in
+  * ...production mode (`NODE_ENV` is not set to 'production'). This lets you
+  * ...configure different settings based on the environment.
+  * When `NODE_ENV` is set to 'production', the base URL is set to
+  * ...`/abthomas3/web287/Projects/Portfolio/'. Change to '/' for all before
+  * ...setting up Netlify.
+  * 
+  * PROPERTIES:
+  * - base: specs the base URL for project. Will be prepended to all asset URLs (e.g. images, css)
+  *     and is used to resolve assets when site is deployed to a subdirectory.
+  * - build: contains options related to build process.
+  * - rollupOptions: contains options for the Rollup bundler Vite uses under the hood.
+  *     Spec entry points for project's main HTML files here.
+  * - define: lets you define global constants that can be accessed elsewhere in code. 
+  *     By defining a '%BASE_URL%' constant that resolves to '/' in production mode I can use
+  *     the constant in the HTML (see nav menu) and the correct path will be passed in.
+  * 
+  * BEFORE NETLIFY SETUP: SWITCH TO SINGLE ROOT DEFINITION AND PUSH TO GITHUB:
+  * export default {
+  *   base: '/',
+  *   build: {
+  *     rollupOptions: {
+  *       input: {
+  *         main: 'index.html',
+  *         portfolio: 'pages/portfolio.html',
+  *         contact: 'pages/about.html',
+  *         resume: 'pages/resume.html'
+  *       }
+  *     },
+  *     define: {
+  *       '%BASE_URL%': '/'
+  *     }
+  *   }
+  * }
+  * 
+*/
+
+
+
+
 // OLD ATTEMPTS:
-// export default {
-//   base: process.env.NODE_ENV === 'production' ? '/abthomas3/web287/Projects/Portfolio/' : '/',
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: 'index.html',
-//         portfolio: 'pages/portfolio.html',
-//         contact: 'pages/contact.html',
-//         resume: 'pages/resume.html'
-//       }
-//     }
-//   }
-// }
 
 // THIS PATHING ONLY WORKS FOR WTCC SERVER
 // export default {
